93da8445a575b55e24f353489605891e1aa4665b
operator08 <33365967+operator08@users.noreply.github.com>
2024-10-11 09:52:00 +0300

    Time: 403 ms (7.91%) | Memory: 58.5 MB (83.85%) - LeetSync

---
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        vector<int> targetTime = times[targetFriend];
+
+        // Sorts according to times[i][0], which represents arrival time in this case
+        sort(times.begin(), times.end());
+
+        int n = times.size();
+        vector<int> chairTime(n);
+
+        for (auto time : times) {
+            for (int i = 0; i < n; i++) {
+                if (chairTime[i] <= time[0]) {
+                    chairTime[i] = time[1];
+                    if (time == targetTime) return i;
+                    break;
+                }
+            }
+        }   
+        return 1;
+    }
+};
