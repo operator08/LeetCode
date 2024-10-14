389685c91ccb19cc9ce123c9fb6b5f5e81bf3934
operator08 <33365967+operator08@users.noreply.github.com>
2024-10-14 23:51:42 +0300

    Time: 0 ms (100.00%) | Memory: 8.5 MB (72.31%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.cpp
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.cpp
@@ -1,33 +1,49 @@
 class Solution {
 public:
-    bool areSentencesSimilar(string sentence1, string sentence2) {
-        istringstream stream1(sentence1);
-        istringstream stream2(sentence2);
-        
-        vector<string> words1;
-        vector<string> words2;
-        string word;
-        
-        while (stream1 >> word) {
-            words1.push_back(word);
+    vector<string> splitIntoWords(string &sentence) {
+        vector<string> words;
+
+        string currentWord;
+
+        for (char c : sentence) {
+            if (isspace(c)) {
+                if (!currentWord.empty()) {
+                    words.push_back(currentWord);
+                    currentWord.clear();	
+                }
+                
+            } else {
+                currentWord += c;
+            }
         }
 
-        while (stream2 >> word) {
-            words2.push_back(word);
+        // Add the last word to the vector
+        if (!currentWord.empty()) {
+            words.push_back(currentWord);	
         }
+
+        return words;
+    }
+
+    bool areSentencesSimilar(string sentence1, string sentence2) {
+        vector<string> words1 = splitIntoWords(sentence1);
+        vector<string> words2 = splitIntoWords(sentence2);
 
         int n1 = words1.size();
         int n2 = words2.size();
 
+        // Check for similar prefixes
         int i = 0, j = 0;
         while (i < n1 && i < n2 && words1[i] == words2[i]) {
             i++;
-        }
+        }	
 
-        while (j < (n1 - i) && j < (n2 - i) && words1[n1 - 1 - j] == words2[n2 - 1 - j]) {
+        // Check for similar suffixes
+        while (j < n1 - i && j < n2 - i && words1[n1 - 1 - j] == words2[n2 - 1 - j]) {
             j++;
         }
 
+        // One sentence must consist of only the common words
         return (i + j >= min(n1, n2));
     }
 };
