0ef44bd267cbc044a59dc7ebb8680d3422043360
operator08 <33365967+operator08@users.noreply.github.com>
2024-10-22 14:07:20 +0300

    Time: 7 ms (99.90%) | Memory: 157.3 MB (5.08%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.cpp
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.cpp
@@ -1,33 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+typedef long long ll;
 class Solution {
 public:
     long long kthLargestLevelSum(TreeNode* root, int k) {
-        vector<long long>ans; // to store the level sum
-        queue<TreeNode*>q; // for level order traversal
+        vector<ll> ans;
+
+        queue<TreeNode*> q;
         q.push(root);
-        while(!q.empty()){
-            long long size = q.size(),sum = 0;
-            for(int i=0;i<size;i++){
-                TreeNode *temp = q.front();
+
+        while (!q.empty()) {
+            ll sum = 0;
+            ll size = q.size();
+            for (ll i = 0; i < size; i++) {
+                TreeNode* tmp = q.front();
                 q.pop();
-                if(temp->left)q.push(temp->left);
-                if(temp->right)q.push(temp->right);
-                sum += (long long)temp->val;
+
+                if (tmp->left)  q.push(tmp->left);
+                if (tmp->right) q.push(tmp->right);
+
+                sum += (ll) tmp->val;
             }
+
             ans.push_back(sum);
-        }
-        sort(ans.begin(),ans.end()); // sorting the ans vector
+        } 
+
+        sort(ans.begin(), ans.end());
+
         int n = ans.size();
-        return (n >= k)?ans[n-k] : -1; // returning the result
+        return (n >= k) ? ans[n - k] : -1;
+        // 0 1 2
     }
 };
