134bae290ceda5dfe5e8b03f92a8a9fcd3088f9f
operator08 <33365967+operator08@users.noreply.github.com>
2024-11-13 18:03:15 +0300

    Time: 0 ms (100.00%) | Memory: 7.8 MB (68.61%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -3,7 +3,6 @@ public:
     bool isValid(string s) {
         int n = s.size();
         stack<char> paren;
-        if (n % 2 == 1) return false;
         for (int i = 0; i < n; i++) {
             char curChar = s[i];
             if (curChar == '(' || curChar == '[' || curChar == '{') {
