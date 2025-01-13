1676d1299e9bfa249122b646ccfab9121d174379
operator08 <33365967+operator08@users.noreply.github.com>
2025-01-13 23:03:12 +0300

    Time: 48 ms (41.15%) | Memory: 32.5 MB (37.69%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -7,11 +7,8 @@ public:
             freq[s[i]]++;
         }
         int res = 0;
-        for (int i = 0; i < n; i++){
-            if (freq[s[i]] != 0) {
-                res += freq[s[i]] % 2 == 0 != 0 ? 2 : 1;
-                freq[s[i]] = 0;
-            }
+        for (auto& f : freq) {
+            res += f.second % 2 == 0 ? 2 : 1;
         }
         return res;
     }
