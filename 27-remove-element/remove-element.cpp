6d1798b0ed76f0d9bebb6fa1f3ed8695773741f7
operator08 <33365967+operator08@users.noreply.github.com>
2025-01-25 13:58:30 +0300

    Time: 0 ms (100.00%) | Memory: 11.7 MB (40.13%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int writer = 0;
-        for (int reader = 0; reader < nums.size(); reader++) {
-            if (nums[reader] != val) {
-                nums[writer++] = nums[reader];
+        int n = nums.size();
+        int k = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != val) k++;
+        }
+        for (int i = 0; i < k; i++) {
+            if (nums[i] == val) {
+                int p = i;
+                for (int j = i + 1; j < n; j++) {
+                    if (nums[j] != val) {
+                        swap(nums[j], nums[p++]);
+                    }
+                }
             }
-        }    
-        return writer;
+        }
+        return k;
     }
 };
