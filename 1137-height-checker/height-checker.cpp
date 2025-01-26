4f298585e7210d885ca4e9f1257c72da59265208
operator08 <33365967+operator08@users.noreply.github.com>
2025-01-26 10:37:11 +0300

    Time: 0 ms (100.00%) | Memory: 11.5 MB (28.44%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -3,7 +3,13 @@ public:
     int heightChecker(vector<int>& heights) {
         int n = heights.size();
         vector<int> h(heights);
-        sort(h.begin(), h.end());
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (h[i] > h[j]) {
+                    swap(h[i], h[j]);
+                }
+            }
+        }
         int res = 0;
         for (int i = 0; i < n; i++) {
             if (h[i] != heights[i]) res++;
