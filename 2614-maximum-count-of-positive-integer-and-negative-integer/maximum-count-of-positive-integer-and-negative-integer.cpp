ebc299fb5298c1fdcd807b3488a5d1c97f8f3d57
operator08 <33365967+operator08@users.noreply.github.com>
2025-03-12 18:31:13 +0300

    Time: 0 ms (100.00%) | Memory: 21.5 MB (10.71%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -2,11 +2,30 @@ class Solution {
 public:
     int maximumCount(vector<int>& nums) {
         int n = nums.size();
-        int pos = 0, neg = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0)      pos++;
-            else if (nums[i] < 0) neg++;
-        }    
-        return max(pos, neg);
+        int i = 0, j = 0;
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= 0) {
+                left = mid + 1; 
+            } else if (nums[mid] > 0) {
+                i = mid;
+                right = mid - 1;
+            }
+        }
+        left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= 0) {
+                right = mid - 1;
+            } else if (nums[mid] < 0) {
+                j = mid;
+                left = mid + 1;
+            }
+        }
+        if (nums[n - 1] == 0) i = n;
+        if (nums[0] == 0)     j = -1;
+        return max(n - i, j + 1);
     }
 };
+// get the split mid, if mid == positive, 
