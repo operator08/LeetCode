a809bfea30522d7ce636a92803e87fa40dde6ec6
operator08 <33365967+operator08@users.noreply.github.com>
2025-04-02 10:20:37 +0300

    Time: 0 ms (100.00%) | Memory: 21.4 MB (99.40%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
@@ -3,20 +3,14 @@ public:
     using i64 = long long;
     long long maximumTripletValue(vector<int>& nums) {
         int n = nums.size();
-        i64 best = -1;
+        i64 best = 0;
         for (int i = 0; i < n; i++) {
-            i64 cur = nums[i];
             for (int j = i + 1; j < n; j++) {
-                cur -= nums[j];
                 for (int k = j + 1; k < n; k++) {
-                    cur *= nums[k];
-                    best = max(best, cur);
-                    cur /= nums[k];
+                    best = max(best, static_cast<i64> (nums[i] - nums[j]) * nums[k]);
                 }
-                cur += nums[j];
             }
         }
-        if (best < 0) return 0;
         return best;
     }
 };
