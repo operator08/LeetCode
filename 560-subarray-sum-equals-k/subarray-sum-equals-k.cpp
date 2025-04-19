94c3ea2e9f32a0ebf31592854c3d66b5f22c1856
operator08 <33365967+operator08@users.noreply.github.com>
2025-04-19 12:32:47 +0300

    Time: 1926 ms (21.10%) | Memory: 35.8 MB (77.95%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        unordered_map<int, int> freq;
-        freq[0] = 1;
-        int sum = 0, count = 0;
+        vector<int> prefix(n + 1);
         for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            if (freq.count(sum - k)) count += freq[sum - k];
-            freq[sum]++;
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) { // non empty subarrays
+                if (prefix[j] - prefix[i] == k) {
+                    count++;
+                }
+            }
         }
         return count;
     }
