99d8459f29c103ef363aa196d07b09522cb2f070
operator08 <33365967+operator08@users.noreply.github.com>
2025-04-24 10:39:27 +0300

    Time: 65 ms (19.19%) | Memory: 94.2 MB (18.39%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -3,7 +3,7 @@ public:
     int numberOfSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
         unordered_map<int, int> freq;
-        freq[0] = 1;
+        freq[0] = 1; // for the contigous subarrays that start from the beginning
         int res = 0, sum = 0;
         for (int i = 0; i < n; i++) {
             sum += (nums[i]) & 1;
