ef471db97ced366ca5204d18bf47cfb8143bbb5a
operator08 <33365967+operator08@users.noreply.github.com>
2025-05-19 22:06:32 +0300

    Time: 0 ms (100.00%) | Memory: 8 MB (51.42%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        static vector<int> memo(38, 0);
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+        if (!memo[n]) return memo[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+        return memo[n];
+    }
+};
