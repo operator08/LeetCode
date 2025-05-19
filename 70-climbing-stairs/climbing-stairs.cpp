a3e26a88245091ab85b3ca39731192939a63ad7e
operator08 <33365967+operator08@users.noreply.github.com>
2025-05-19 10:38:41 +0300

    Time: 0 ms (100.00%) | Memory: 7.8 MB (71.75%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n + 1);
-        dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n];
+        static vector<int> memo(46, 0);
+        if (n == 0 || n == 1) return 1;
+        if (memo[n] != 0)     return memo[n]; 
+        memo[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        return memo[n];
     }
 };
