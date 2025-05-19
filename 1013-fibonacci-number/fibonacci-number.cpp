0677cb58fcb3ace7a69b039074d3cce36db87af6
operator08 <33365967+operator08@users.noreply.github.com>
2025-05-19 10:44:24 +0300

    Time: 0 ms (100.00%) | Memory: 8 MB (22.35%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
+        static vector<int> memo(31, 0);
         if (n == 1 || n == 0) return n;
-        return fib(n - 1) + fib(n - 2);
+        if (!memo[n]) memo[n] = fib(n - 1) + fib(n - 2);
+        return memo[n];
     }
 };
