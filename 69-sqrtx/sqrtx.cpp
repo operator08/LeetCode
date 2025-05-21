d8c3e21116bf730347f5c1dd0d9b454c4b86260a
operator08 <33365967+operator08@users.noreply.github.com>
2025-05-21 11:15:43 +0300

    Time: 33 ms (5.04%) | Memory: 8.6 MB (51.02%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
+    using i64 = long long;
     int mySqrt(int x) {
-        return floor(sqrt(x));    
+        for (i64 i = 0; i < INT_MAX; i++)
+            if (i * i <= x && (i + 1) * (i + 1) > x)
+                return i;
+        return -1;
     }
 };
