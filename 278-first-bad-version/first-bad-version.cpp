9d71c8cde53ff566b19340d99a6f0b7537b3509e
operator08 <33365967+operator08@users.noreply.github.com>
2025-05-23 00:41:46 +0300

    Time: 2 ms (54.96%) | Memory: 7.8 MB (92.95%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -8,12 +8,11 @@ public:
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (isBadVersion(mid)) {
-                if (!isBadVersion(mid - 1)) return mid;
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         } 
-        return n;
+        return left;
     }
 };
