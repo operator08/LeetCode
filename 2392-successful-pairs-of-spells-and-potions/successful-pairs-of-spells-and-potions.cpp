4cc98cdf9833e4a0a1feb45251cac4fe1ddc6876
operator08 <33365967+operator08@users.noreply.github.com>
2025-05-23 12:44:48 +0300

    Time: 51 ms (22.88%) | Memory: 128.4 MB (8.06%) - LeetSync

---
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    using i64 = long long;
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        int n = spells.size(), m = potions.size();
+        sort(potions.begin(), potions.end());
+        vector<int> res;
+        for (int i = 0; i < n; i++) {
+            int curSpell = spells[i];
+            int count = 0;
+            int left = 0, right = m - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if ((i64) potions[mid] * curSpell >= success) {
+                    count += right - mid + 1;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            res.emplace_back(count);
+        }    
+        return res;
+    }
+};
