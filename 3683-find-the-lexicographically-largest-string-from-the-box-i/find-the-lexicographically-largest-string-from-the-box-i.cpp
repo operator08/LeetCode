2a56b80eeb0fc25ddcb14cdf305ca08500993501
operator08 <33365967+operator08@users.noreply.github.com>
2025-06-05 00:03:53 +0300

    Time: 73 ms (44.11%) | Memory: 169.3 MB (25.59%) - LeetSync

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     string answerString(string word, int numFriends) {
+        if (numFriends == 1) return word;
         string bestSub = "";
         int n = word.size();
-        if (numFriends == 1) return word;
         for (int i = 0; i < n; i++) {
             string currentSub = word.substr(i, n - numFriends + 1);
             bestSub = max(bestSub, currentSub);
