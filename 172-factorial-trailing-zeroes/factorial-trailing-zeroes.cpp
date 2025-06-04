80bda41f827229de879d9dc7725512b0a971b94c
operator08 <33365967+operator08@users.noreply.github.com>
2025-06-05 00:38:22 +0300

    Time: 0 ms (100.00%) | Memory: 8.2 MB (16.31%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        if (n == 0) return 0;
-        return n / 5 + trailingZeroes(n / 5);
+        int fiveCount = 0;
+        for (int i = 5; n / i > 0; i *= 5) {
+            fiveCount += n / i;
+        }
+        return fiveCount;
     }
 };
