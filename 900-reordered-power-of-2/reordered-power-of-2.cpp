f9fd86dabf2c007bba3acc1b8ac7646c9014a1b2
operator08 <33365967+operator08@users.noreply.github.com>
2025-08-10 15:14:16 +0300

    Time: 11 ms (11.80%) | Memory: 9.7 MB (14.75%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     bool reorderedPowerOf2(int n) {
       unordered_map<string, bool> umap;
-      for (int i = 0; i < 30; i++) {
+      for (int i = 0; i <= 30; i++) { // 2^30 = ~10^9
         string num = to_string(1 << i);
         sort(num.begin(), num.end());
         umap[num] = 1;
+        cout << num << ' ';
       }
       string num = to_string(n); 
       sort(num.begin(), num.end());
